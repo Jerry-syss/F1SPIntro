c86331426fc74941ce653e170ff27694d0d4ddb0
Jerry-syss <s11357@syss.edu.hk>
2026-01-10 17:24:23 +0800

    Update check-js.js

--- cheack/check-js.js
+++ cheack/check-js.js
@@ -1,1 +1,15 @@
-
+window.onload = function()
+{
+    const ua = navigator.userAgent.toLowerCase();
+    const isMobile =
+        /iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(ua)
+        || window.innerWidth < 768;
+    if (isMobile)
+    {
+        window.location.href = "../home-phone/home-phone.html";//phone
+    }
+    else
+    {
+        window.location.href = "../home-comp/home-comp.html";//computer
+    }
+};
