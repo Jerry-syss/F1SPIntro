ec55393b7e85ab5d34ad631c68903a605666cfa7
Jerry-syss <s11357@syss.edu.hk>
2026-03-25 18:29:02 +0800

    Update home.js

--- database/js/home.js
+++ database/js/home.js
@@ -1,6 +1,6 @@
 var home = `
             <h1>歡迎使用 SYSS 中一野餐 URL</h1>
-            <img src="./other/syss.png" alt="error:404|School Logo">
+            <img src="datebase/other/syss.png" alt="error:404|School Logo">
             <button type="button" onclick="changePage('i')">大拗門資料 📑</button>
             <button type="button" onclick="changePage('b')">BBQ 資料 🔥</button>
             <button type="button" onclick="changePage('m')">價格參考 💰</button>
