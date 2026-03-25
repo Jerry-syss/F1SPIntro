d0844a78567b42368dcaf303df74e652a879dfbd
Jerry-syss <s11357@syss.edu.hk>
2026-03-25 18:22:16 +0800

    change-to-datebase

---
+++ database/js/time.js
@@ -0,0 +1,40 @@
+function showTime()
+{
+    var nowTime_GMT = new Date();
+    var nowTime = nowTime_GMT.toString().replace(/\sGMT.*$/,"");
+    var nowHour = nowTime_GMT.getHours() + 1;
+    if(nowHour > 3 && nowHour <= 6)
+    {
+        var words = "清晨 ";
+        document.getElementById('time').textContent = words + nowTime;
+    }
+    if(nowHour > 6 && nowHour <= 10)
+    {
+        var words = "早上 ";
+        document.getElementById('time').textContent = words + nowTime;
+    }
+    if(nowHour > 10 && nowHour <= 13)
+    {
+        var words = "中午 ";
+        document.getElementById('time').textContent = words + nowTime;
+    }
+    if(nowHour > 13 && nowHour <= 18)
+    {
+        var words = "下午 ";
+        document.getElementById('time').textContent = words + nowTime;
+    }
+    if(nowHour > 18 && nowHour <= 23)
+    {
+        var words = "晚上 ";
+        document.getElementById('time').textContent = words + nowTime;
+    }
+    if(nowHour > 23 && nowHour <= 3)
+    {
+        var words = "深夜 ";
+        document.getElementById('time').textContent = words + nowTime;
+    }
+}
+showTime();
+setInterval(function(){
+	showTime();
+},1000);
